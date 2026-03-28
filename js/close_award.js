a976ecd65aa501546540ff0ff4baf8455c21a6c7
Roman Orlov <romaorlov2013@gmail.com>
2026-03-28 13:56:24 +0100

    connect js close_award file

--- js/close_award.js
+++ js/close_award.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const award = document.querySelector(".award__images");
+const award = document.querySelector(".award__icons");
 
 award.addEventListener("click", () => {
   document.querySelector(".award").remove();
